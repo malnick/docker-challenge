fe26954dc74a32493b02a5568298b136c2bbb4c6
Jeff <malnick@gmail.com>
2014-06-13 09:56:52 -0700

    added python deps

--- manifests/deploy_nagios.pp
+++ manifests/deploy_nagios.pp
@@ -11,8 +11,27 @@ file { '/usr/local/bin/check_docker':
 file { '/etc/init/docker.conf':
   ensure => file,
   source => 'puppet:///modules/nagios/docker.conf',
+  owner => 'root',
+  group => 'root',
   require => File['/usr/local/bin/check_docker'],
 }
 
+# Docker plugin need some python deps
+$docker_plugin_pkgs = ['docker-py','nagiosplugin']
 
+package { 'python-setuptools':
+  ensure => present,
 }
+
+exec {"/usr/bin/pip install -r ${docker_plugin_pks}":
+  creates => '',
+  require => Packge ['python-setuptools'],
+}
+
+#package { $docker_plugin_pkgs:
+#  ensure => present,
+#  provider => 'pip',
+#}
+
+
+
